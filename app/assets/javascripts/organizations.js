60fb0852c378596e742eaa3f6c0d3690e998d536
Hon Lam <lamh85@gmail.com>
2015-07-21 22:23:58 -0700

    Started HTML-Angular integration.

--- app/assets/javascripts/organizations.js
+++ app/assets/javascripts/organizations.js
@@ -2,4 +2,8 @@
 
   var app = angular.module("Terrain", ['ng-rails-csrf']);
 
+  FormController = app.controller("FormController", ['$scope', function($scope) {
+    $scope.filterNumbers = [1];
+  }])
+
 })(); // End whole file's closure
